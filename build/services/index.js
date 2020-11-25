03c4cbe6ea8a3cc2eb972f44f8fc40d4c70ca838
georgi.enev <georgi.enev@catenate.com>
2020-11-25 15:38:58 +0200

    removed build folder

--- build/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.initializeServices = void 0;
-const product_service_1 = require("./product/product.service");
-const initializeServices = (configurations, repositories) => ({
-    productService: product_service_1.instantiateProductService(configurations, repositories.products),
-});
-exports.initializeServices = initializeServices;
-//# sourceMappingURL=index.js.map
